921a7271ba4e7cfe3a89560c9759e8142ac941cb
Roopesh Verma <roopeshv.30@gmail.com>
2024-04-01 11:41:19 +0530

    Add Idle, Rotation and Shooting states

---
+++ Assets/Scripts/Enemy/States/IdleState.cs
@@ -0,0 +1,22 @@
+namespace StatePattern.Enemy.States
+{
+    public class IdleState : IState
+    {
+        public OnePunchManController Owner { get; set; }
+        
+        public void OnStateEnter()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnStateStay()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnStateExit()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

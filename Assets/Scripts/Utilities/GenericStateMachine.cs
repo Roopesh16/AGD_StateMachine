fc84dcc0297ef862361e6ad0abb66305297aa751
Roopesh Verma <roopeshv.30@gmail.com>
2024-04-03 11:17:13 +0530

    Add Generic State machine

---
+++ Assets/Scripts/Utilities/GenericStateMachine.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using StateMachine;
+using StatePattern.Enemy;
+
+namespace StatePattern.Utilities
+{
+    public class GenericStateMachine<T> where T : EnemyController
+    {
+        protected T Owner;
+        protected IState currentState;
+        protected Dictionary<States, IState> states = new();
+
+        public GenericStateMachine(T Owner) => this.Owner = Owner;
+
+        public void Update() => currentState?.Update();
+
+        public void ChangeState(States newState) => ChangeState(states[newState]);
+
+        protected void ChangeState(IState newState)
+        {
+            currentState?.OnStateEnter();
+            currentState = newState;
+            currentState?.OnStateExit();
+        }
+
+        protected void SetOwner()
+        {
+            foreach (IState state in states.Values)
+            {
+                state.Owner = Owner;
+            }
+        }
+    }
+}

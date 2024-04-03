44d33a492a6457bb5cc130e004bf5a5c5891970c
Roopesh Verma <roopeshv.30@gmail.com>
2024-04-03 11:16:59 +0530

    Update IStateMac

--- Assets/Scripts/StateMachine/IStateMachine.cs
+++ Assets/Scripts/StateMachine/IStateMachine.cs
@@ -4,17 +4,9 @@ using UnityEngine;
 
 namespace StatePattern.StateMachine
 {
-    public interface IStateMachine
+   public interface IStateMachine
     {
         public void ChangeState(States newState);
     }
 
-    public enum States
-    {
-        IDLE,
-        ROTATING,
-        SHOOTING,
-        PATROLLING,
-        CHASING
-    }
 }

56da7bb601c18054d68adc3fbf6508301ac664b0
Roopesh Verma <roopeshv.30@gmail.com>
2024-04-03 12:00:41 +0530

    Inherit from generic SM

--- Assets/Scripts/Enemy/PatrolMan/PatrolManStateMachine.cs
+++ Assets/Scripts/Enemy/PatrolMan/PatrolManStateMachine.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections.Generic;
 using StateMachine;
+using StatePattern.StateMachine;
+using StatePattern.Utilities;
 
 namespace StatePattern.Enemy
 {
-    public class PatrolManStateMachine : IStateMachine
+    public class PatrolManStateMachine : GenericStateMachine<PatrolManController>
     {
         private PatrolManController Owner;
         private IState currentState;
-        protected Dictionary<States, IState> States = new Dictionary<States, IState>();
+        protected Dictionary<States, IState> states = new Dictionary<States, IState>();
 
-        public PatrolManStateMachine(PatrolManController Owner)
+        public PatrolManStateMachine(PatrolManController Owner) : base(Owner)
         {
             this.Owner = Owner;
             CreateStates();
@@ -18,15 +20,15 @@ namespace StatePattern.Enemy
 
         private void CreateStates()
         {
-            States.Add(StateMachine.States.IDLE, new IdleState(this));
-            States.Add(StateMachine.States.PATROLLING, new PatrollingState(this));
-            States.Add(StateMachine.States.CHASING, new ChasingState(this));
-            States.Add(StateMachine.States.SHOOTING, new ShootingState(this));
+            states.Add(States.IDLE, new IdleState<PatrolManController>(this));
+            states.Add(States.PATROLLING, new PatrollingState<PatrolManController>(this));
+            states.Add(States.CHASING, new ChasingState<PatrolManController>(this));
+            states.Add(States.SHOOTING, new ShootingState<PatrolManController>(this));
         }
 
         private void SetOwner()
         {
-            foreach (IState state in States.Values)
+            foreach (IState state in states.Values)
             {
                 state.Owner = Owner;
             }
@@ -41,6 +43,6 @@ namespace StatePattern.Enemy
             currentState?.OnStateEnter();
         }
 
-        public void ChangeState(States newState) => ChangeState(States[newState]);
+        public void ChangeState(States newState) => ChangeState(states[newState]);
     }
 }

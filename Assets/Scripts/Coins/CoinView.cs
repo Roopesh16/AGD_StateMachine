09258399ae84607f1b8789c36015248c3256d651
Roopesh Verma <roopeshv.30@gmail.com>
2024-04-07 20:44:38 +0530

    Add coin view

--- Assets/Scripts/Coins/CoinView.cs
+++ Assets/Scripts/Coins/CoinView.cs
@@ -1,14 +1,43 @@
 using System;
+using System.Threading.Tasks;
 using UnityEngine;
 using DG.Tweening;
+using StatePattern.Main;
+using StatePattern.UI;
 
 namespace Coins
 {
     public class CoinView : MonoBehaviour
     {
+        [SerializeField] private float minDistance = 1f;
+        [SerializeField] private float moveDuration = 10f;
+        [SerializeField] private int multiplier = 500;
+        
+        private Vector3 playerPosition;
+        private bool canMove = false;
         private void Start()
         {
+            playerPosition = GameService.Instance.PlayerService.GetPlayer().Position;
             transform.DORotate(transform.up, 2f, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Incremental);
+        }
+
+        private void Update()
+        {
+            if (Vector3.Distance(playerPosition, transform.position) <= minDistance && !canMove)
+            {
+                canMove = true;
+                MoveCoin();
+                
+            }
+        }
+        
+        private async void MoveCoin()
+        {
+            transform.DOMove(playerPosition,moveDuration, false).SetEase(Ease.OutQuad);
+            GameService.Instance.UIService.UpdateCointCount();
+            await Task.Delay((int)moveDuration * multiplier);
+            DOTween.PauseAll();
+            Destroy(gameObject);
         }
     }
 }

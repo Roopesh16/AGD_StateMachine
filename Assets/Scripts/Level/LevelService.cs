fbaaff321b206a77d9b123b805c0b22050246c0c
Roopesh Verma <roopeshv.30@gmail.com>
2024-04-07 22:39:44 +0530

    Destroy previous level and send

--- Assets/Scripts/Level/LevelService.cs
+++ Assets/Scripts/Level/LevelService.cs
@@ -9,24 +9,41 @@ namespace StatePattern.Level
     public class LevelService
     {
         private List<LevelScriptableObject> levelScriptableObjects;
-
+        private int currentLevel;
+        private LevelScriptableObject levelData;
+        private GameObject levelPrefab;
+        
         public LevelService(List<LevelScriptableObject> levelScriptableObjects)
         {
             this.levelScriptableObjects = levelScriptableObjects;
             SubscribeToEvents();
         }
 
-        private void SubscribeToEvents() => GameService.Instance.EventService.OnLevelSelected.AddListener(LoadLevel);
+        private void SubscribeToEvents()
+        {
+            GameService.Instance.EventService.OnLevelSelected.AddListener(LoadLevel);
+            GameService.Instance.EventService.OnLevelEnded.AddListener(ClearPreviousLevel);
+        }
 
         private void UnsubscribeToEvents() => GameService.Instance.EventService.OnLevelSelected.RemoveListener(LoadLevel);
 
         public void LoadLevel(int levelID)
         {
-            var levelData = levelScriptableObjects.Find(levelSO => levelSO.ID == levelID);
-            Object.Instantiate(levelData.LevelPrefab);
-            UnsubscribeToEvents();
+            currentLevel = levelID;
+            levelData = levelScriptableObjects.Find(levelSO => levelSO.ID == levelID);
+            levelPrefab = Object.Instantiate(levelData.LevelPrefab);
         }
 
         public List<EnemyScriptableObject> GetEnemyDataForLevel(int levelId) => levelScriptableObjects.Find(level => level.ID == levelId).EnemyScriptableObjects;
+
+        public int GetCurrentLevel() => currentLevel;
+
+        private void ClearPreviousLevel()
+        {
+            if (levelPrefab != null)
+            {
+                Object.Destroy(levelPrefab);
+            }
+        }
     }
 }

2fee551963d5802d5f2dd58c2025efcb12618231
Roopesh Verma <roopeshv.30@gmail.com>
2024-04-09 16:39:12 +0530

    Add Titanis Controller

---
+++ Assets/Scripts/Enemy/Titanis/TitanisController.cs
@@ -0,0 +1,39 @@
+using StatePattern.Player;
+using StatePattern.StateMachine;
+
+namespace StatePattern.Enemy.Titanis
+{
+    public class TitanisController : EnemyController
+    {
+        private TitanisStateMachine stateMachine;
+        
+        public TitanisController(EnemyScriptableObject enemyScriptableObject) : base(enemyScriptableObject)
+        {
+            enemyView.SetController(this);
+            CreateStateMachine();
+            stateMachine.ChangeState(States.IDLE);
+        }
+
+        private void CreateStateMachine() => stateMachine = new TitanisStateMachine(this);
+
+        public override void UpdateEnemy()
+        {
+            if(currentState == EnemyState.DEACTIVE)
+                return;
+            
+            stateMachine?.Update();
+        }
+
+        public override void PlayerEnteredRange(PlayerController targetToSet)
+        {
+            base.PlayerEnteredRange();
+            stateMachine.ChangeState(States.ROARING);
+        }
+
+        public override void PlayerExitedRange()
+        {
+            stateMachine.ChangeState(States.IDLE);
+        }
+        
+    }
+}
